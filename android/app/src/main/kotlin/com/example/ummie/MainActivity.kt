6bdec1911072cc065c5e733c0279f4d0ad7d62d2
digmouse <digmouse@digmouses-mbp.home>
2023-01-16 18:10:44 -0500

    Initialize Ummie

---
+++ android/app/src/main/kotlin/com/example/ummie/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ummie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Ummie());
+}
+
+class Ummie extends StatelessWidget {
+  const Ummie({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
